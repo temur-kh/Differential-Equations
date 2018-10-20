a8249156bef49ddc26585e80862fc644d11d56e1
Temur <tim_1998@bk.ru>
2018-10-20 15:40:01 +0300

    TabBar added. Implementation of table needed

--- course_assignment/window.py
+++ course_assignment/window.py
@@ -2,8 +2,9 @@ from methods import *
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import sys
 import tkinter as tk
+from tkinter_tabs import Tab, TabBar
+from sys import exit
 e = 2.71828182846
 
 
@@ -52,7 +53,8 @@ class Variant:
 
 
 class Plot:
-    def draw(self, x0=Variant.x0, y0=Variant.y0, x=Variant.x, h=Variant.h, ax=None):
+    @staticmethod
+    def draw(x0=Variant.x0, y0=Variant.y0, x=Variant.x, h=Variant.h, ax=None):
         """
         Draw the graph of function using given parameters.
         :param x0: initial position on x-axis
@@ -91,14 +93,16 @@ class Plot:
         df.plot(x='x', y='Euler Method', color='Blue', ax=ax)
         df.plot(x='x', y='Improved Euler Method', color='Red', ax=ax)
         df.plot(x='x', y='Runge-Kuffa Method', color='LightGreen', ax=ax)
+        return df
 
     def redraw(self, x0, y0, x, h, canvas):
         x0_new, y0_new, x_new, h_new = float(x0.get()), float(y0.get()), float(
             x.get()), float(h.get())
         if x0_new < x_new and (x_new - x0_new) / h_new <= 10000:
             plt.gca().clear()
-            self.draw(x0_new, y0_new, x_new, h_new, plt.gca())
+            df = self.draw(x0_new, y0_new, x_new, h_new, plt.gca())
             canvas.draw()
+            return df
 
 
 class Window:
@@ -108,63 +112,73 @@ class Window:
         :param master: the window where the content should be placed
         """
         self.root = master
+        bar = TabBar(self.root, "graph")
         fun_label = tk.Label(self.root, text="Differential Equation: y'=xy^2-3xy", font=("Courier", 30))
         fun_label.pack()
 
-        # Draw graph and put its figure into a canvas
-        self.plot = Plot()
-        self.plot.draw()
+        # Create form below
+        form = tk.Frame(self.root)
 
-        canvas = FigureCanvasTkAgg(plt.gcf(), master=self.root)
-        canvas.draw()
-        canvas.get_tk_widget().pack(side=tk.TOP,
-                                    fill=tk.BOTH,
-                                    expand=1)
-        # Create a toolbar containing all variables and button.
-        bar = tk.Frame(self.root)
-
-        x0_label = tk.Label(bar, text="x0")
+        x0_label = tk.Label(form, text="x0")
         x0_label.pack()
 
-        x0 = tk.Entry(bar, width=30)
+        x0 = tk.Entry(form, width=30)
         x0.insert(tk.END, str(Variant.x0))
         x0.pack()
 
-        y0_label = tk.Label(bar, text="y0")
+        y0_label = tk.Label(form, text="y0")
         y0_label.pack()
 
-        y0 = tk.Entry(bar, width=30)
+        y0 = tk.Entry(form, width=30)
         y0.insert(tk.END, str(Variant.y0))
         y0.pack()
 
-        x_label = tk.Label(bar, text="X")
+        x_label = tk.Label(form, text="X")
         x_label.pack()
 
-        x = tk.Entry(bar, width=30)
+        x = tk.Entry(form, width=30)
         x.insert(tk.END, str(Variant.x))
         x.pack()
 
-        h_label = tk.Label(bar, text="Grid step")
+        h_label = tk.Label(form, text="Grid step")
         h_label.pack()
 
-        h = tk.Entry(bar, width=30)
+        h = tk.Entry(form, width=30)
         h.insert(tk.END, str(Variant.h))
         h.pack()
 
-        button = tk.Button(master=bar,
+        button = tk.Button(master=form,
                            text='Plot',
                            command=lambda: self.plot.redraw(x0, y0, x, h, canvas),
                            bg="white")
         button.pack()
 
-        button = tk.Button(master=bar,
+        button = tk.Button(master=form,
                            text='Quit',
-                           command=sys.exit,
+                           command=exit,
                            bg="white")
         button.pack()
-
-        bar.pack(side=tk.BOTTOM, fill=tk.X)
-
         # Print author's name
-        author = tk.Label(bar, text="Author: Temur Kholmatov B17-5")
+        author = tk.Label(form, text="Author: Temur Kholmatov B17-5")
         author.pack(side=tk.RIGHT, fill=tk.X)
+
+        form.pack(side=tk.BOTTOM, fill=tk.X)
+
+        # Create graph tab
+        graph = Tab(self.root, 'graph')
+
+        # Draw graph and put its figure into a canvas
+        self.plot = Plot()
+        self.plot.draw()
+        canvas = FigureCanvasTkAgg(plt.gcf(), master=graph)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP,
+                                    fill=tk.BOTH,
+                                    expand=tk.YES)
+
+        # Create table tab
+        table = Tab(self.root, 'table')
+
+        bar.add(graph)
+        bar.add(table)
+        bar.show()
